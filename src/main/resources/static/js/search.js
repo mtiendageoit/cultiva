deae38e6428a130f79a72b980e1958c32b30ce5c
Meliton Tienda Avila <mtienda@geoit.com.mx>
2024-07-02 21:10:10 -0600

    Se agrega autocomplete de google

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,10 +1,30 @@
 const Search = ((element) => {
   const removeSearchBtn = $('#search-remove-search');
   const searchInput = $('#search-input');
+  const gAutocompleteOptions = {
+    componentRestrictions: { country: "mx" },
+    fields: ["geometry", "name"],
+    strictBounds: false,
+  };
+  const googleAutocomplete = new google.maps.places.Autocomplete(searchInput[0], gAutocompleteOptions);
 
   function init() {
     removeSearchBtn.click(onRemoveSearchClick);
     searchInput.keyup(onSearchInputKeyup);
+    googleAutocomplete.addListener("place_changed", onAutocompleteChange);
+  }
+
+  function onAutocompleteChange() {
+    const place = googleAutocomplete.getPlace();
+    if (place.geometry && place.geometry.viewport) {
+      const viewport = place.geometry.viewport;
+
+      const extent = ol.extent.boundingExtent([
+        ol.proj.fromLonLat([viewport.getSouthWest().lng(), viewport.getSouthWest().lat()]),
+        ol.proj.fromLonLat([viewport.getNorthEast().lng(), viewport.getNorthEast().lat()])
+      ]);
+      olMap.getView().fit(extent);
+    }
   }
 
   function onRemoveSearchClick() {
